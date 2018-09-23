3eb8185942842f69247d5633506469aef7be8cb8
maksstef <38255643+maksstef@users.noreply.github.com>
2018-09-23 16:22:08 +0300

    sorter

--- src/index.js
+++ src/index.js
@@ -1,30 +1,54 @@
 class Sorter {
   constructor() {
     // your implementation
+    this.store = [];
+    this.compareFunction = this.defaultNC;
   }
 
+  defaultNC(a,b)
+  {
+    return a-b;
+  }
   add(element) {
     // your implementation
+    this.store.push(element);
   }
 
   at(index) {
     // your implementation
+    return this.store[index];
   }
 
   get length() {
     // your implementation
+    return this.store.length;
   }
 
   toArray() {
     // your implementation
+    return this.store;
   }
 
   sort(indices) {
     // your implementation
+    indices = indices.sort(this.defaultNumbersCompare);
+ let sortedArray = this.store.filter((item, index) => {
+     if (indices.some((element) => {
+             return index===element
+         })
+     ) {
+         return true;
+     }
+ }).sort(this.compareFunction);
+
+ indices.forEach((item, index) => {
+     this.store[item] = sortedArray[index];
+ });
   }
 
   setComparator(compareFunction) {
     // your implementation
+      this.compareFunction = compareFunction;
   }
 }
 
